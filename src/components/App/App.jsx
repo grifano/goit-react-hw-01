bb254539db46753fa7f8f90135e091aa8ef54412
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2024-06-13 23:07:56 +0200

    complete task 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import Profile from "../Profile/Profile";
 import css from "../App/App.module.css";
 import { username, tag, location, avatar, stats } from "../../userData.json";
+import friends from "../../friends.json";
+import FriendsList from "../FriendsList/FriendsList";
 
 export default function App() {
   return (
@@ -12,6 +14,7 @@ export default function App() {
         imageUrl={avatar}
         stats={stats}
       />
+      <FriendsList friends={friends} />
     </div>
   );
 }
